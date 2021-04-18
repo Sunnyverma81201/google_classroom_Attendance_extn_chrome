a0707f0d87784e05bce430ca0c0c4dfffdc23870
Apoorv Verma <40027838+Sunnyverma81201@users.noreply.github.com>
2021-04-18 13:39:37 +0530

    added the payload script and implementes popup.js and some UI changes

---
+++ attendance.js
@@ -0,0 +1,9 @@
+var x = document.getElementsByClassName("ZjFb7c");
+var list = [];
+var students = "";
+for (i=0;i<x.length;i++){
+    console.log(x[i].innerText);
+}
+
+// var blob = new Blob(["This is my first text."], {type: "text/plain;charset=utf-8"});
+//    saveAs(blob, "Attendance.txt");
